2d5bf6b5949337f39bc7f2d4d30e0396f51619dc
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-03-07 23:49:34 -0500

    Code clean

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,81 +1,59 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import { CardMedia } from '@mui/material';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import  { Redirect } from 'react-router-dom'
-import { Navigate, useNavigate } from 'react-router-dom';
-
-// function Copyright(props) {
-//   return (
-//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-//       {'Copyright © '}
-//       <Link color="inherit" href="https://mui.com/">
-//         Your Website
-//       </Link>{' '}
-//       {new Date().getFullYear()}
-//       {'.'}
-//     </Typography>
-//   );
-// }
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
-export default function SignInSide({setIsLoggedIn}) {
+export default function SignInSide({ setIsLoggedIn }) {
   const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    // console.log({
-    //   email: data.get('email'),
-    //   password: data.get('password'),
-    // });
     const body = {
-      email: data.get('email'),
-      password: data.get('password'),
-    }
-    axios.post('/api/auth/login', body)
-      .then(res => {
-        // console.log("RESULT______>", res);
-        localStorage.setItem('user_id', res.data.user_id);
-        localStorage.setItem('account_id', res.data.account_id);
-        localStorage.setItem('first_name', res.data.first_name);
-        setIsLoggedIn(true)
-        navigate('/home');
-        //return <Navigate to='/home'/>
-      });
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+    axios.post("/api/auth/login", body).then((res) => {
+      localStorage.setItem("user_id", res.data.user_id);
+      localStorage.setItem("account_id", res.data.account_id);
+      localStorage.setItem("first_name", res.data.first_name);
+      setIsLoggedIn(true);
+      navigate("/home");
+    });
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
-       
+
         <Grid
           item
           xs={false}
           sm={4}
           md={7}
           sx={{
-            backgroundColor: 'black',
-            backgroundImage: 'url(https://media.discordapp.net/attachments/920410019474833488/945696980292755516/png_20220222_100203_0000.png?width=726&height=1027)',
-            backgroundRepeat: 'no-repeat',
+            backgroundColor: "black",
+            backgroundImage:
+              "url(https://media.discordapp.net/attachments/920410019474833488/945696980292755516/png_20220222_100203_0000.png?width=726&height=1027)",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'fit',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "fit",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -83,21 +61,23 @@ export default function SignInSide({setIsLoggedIn}) {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            {/* <Typography component="h1" variant="h4">
-              Welcome to family matters
-            </Typography> */}
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -118,10 +98,6 @@ export default function SignInSide({setIsLoggedIn}) {
                 id="password"
                 autoComplete="current-password"
               />
-              {/* <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              /> */}
               <Button
                 type="submit"
                 fullWidth
@@ -132,18 +108,12 @@ export default function SignInSide({setIsLoggedIn}) {
                 Sign In
               </Button>
               <Grid container>
-                {/* <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid> */}
                 <Grid item>
                   <Link href="/signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
               </Grid>
-              {/* <Copyright sx={{ mt: 5 }} /> */}
             </Box>
           </Box>
         </Grid>
