e37a544134d0e946fba27318163b0f92626f1f03
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-03-10 00:40:27 -0500

    Format indentation and remove unused code

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,51 +1,59 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 
-import Sidebar from './Sidebar';
+import Sidebar from "./Sidebar";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
-
   const username = localStorage.getItem("first_name");
   const navigate = useNavigate();
-  
+
   const signOut = () => {
     localStorage.clear();
-    navigate('/');
-  }
-   
+    navigate("/");
+  };
 
-  return (    
-
+  return (
     <nav class="navbar navbar-expand-lg navbar-dark  static-top">
       <div class="container">
         <a class="navbar-brand" href="#">
-          <h1 id='welcome-msg'>Welcome back {username}!</h1>
+          <h1 id="welcome-msg">Welcome back {username}!</h1>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link " aria-current="page" href="/about">About</a>
+              <a class="nav-link " aria-current="page" href="/about">
+                About
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/family">My Family</a>
+              <a class="nav-link" href="/family">
+                My Family
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/" onClick={signOut} >Sign out</a>
+              <a class="nav-link" href="/" onClick={signOut}>
+                Sign out
+              </a>
             </li>
           </ul>
         </div>
       </div>
-      <Sidebar/>
+      <Sidebar />
     </nav>
-  
-   
-    
-  )
+  );
 }
 
 export default Nav;
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import '../App.css';
+import "../App.css";
 
 function Sidebar() {
   return (
     <header className="header" role="banner">
-  <h1 className="logo">
-    <a href="/home">Family <span>Matters</span></a>
-  </h1>
-  <div className="nav-wrap">
-    <nav className="main-nav" role="navigation">
-      <ul className="unstyled list-hover-slide">
-        <li><a href="/home">Home</a></li>
-        <li><a href="/events">Events</a></li>
-        <li><a href="/lists">Lists</a></li>
-        <li><a href="/meals">Meals</a></li>
-        <li><a href="/recipes">Recipes</a></li>
-        <li><a href="/contacts">Contacts</a></li>
-     </ul>
-    </nav>
-   
-  </div>
-</header>
-  )
+      <h1 className="logo">
+        <a href="/home">
+          Family <span>Matters</span>
+        </a>
+      </h1>
+      <div className="nav-wrap">
+        <nav className="main-nav" role="navigation">
+          <ul className="unstyled list-hover-slide">
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/events">Events</a>
+            </li>
+            <li>
+              <a href="/lists">Lists</a>
+            </li>
+            <li>
+              <a href="/meals">Meals</a>
+            </li>
+            <li>
+              <a href="/recipes">Recipes</a>
+            </li>
+            <li>
+              <a href="/contacts">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
